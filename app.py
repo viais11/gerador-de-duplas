2e5816e391819b8e8439fde7d7963c087f71c1d4
viais11 <pedroviais11@gmail.com>
2025-07-25 14:37:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
 import streamlit as st
 import datetime
 from collections import defaultdict
